dce539a5ad71aaad17361f6dd8966e6a0faa6d44
Paul Matchett <p.matchett80@gmail.com>
2017-03-22 20:36:34 +0000

    adding branch with lagacy bootstrap 3

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -100,14 +100,12 @@ var config = {
     js: {
       start: [
         './node_modules/jquery/dist/jquery.js',
-        './node_modules/tether/dist/js/tether.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
         './js/main.js'
       ],
       filesThatDontNeedCompiled: [
         './node_modules/jquery/dist/jquery.js',
-        './node_modules/tether/dist/js/tether.js',
-        './node_modules/bootstrap/dist/js/bootstrap.js'
+        './node_modules/bootstrap/dist/js/bootstrap.js',
       ],
       dest: './dist/siteFiles/js'
     },
