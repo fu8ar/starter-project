470f85c9843c16540794f426e2e98f60092664fa
Paul Matchett <p.matchett80@gmail.com>
2017-10-09 20:58:25 +0100

    update image paths for verndor files

--- tools/constants.ts
+++ tools/constants.ts
@@ -35,8 +35,8 @@ export const HTMLConfig = {
 // Vendor Config
 export const VendorConfig = {
   libraries: [
-    './node_modules/jquery/dist/jquery.js',
-    './node_modules/tether/dist/js/tether.js',
+    './node_modules/jquery/dist/jquery.slim.js',
+    './node_modules/popper.js/dist/umd/popper.js',
     './node_modules/bootstrap/dist/js/bootstrap.js',
     './node_modules/slick-carousel/slick/slick.js'
   ],
@@ -97,7 +97,7 @@ export const publishConfig = {
   export const FTP_OPTIONS = {
     host:     publishConfig.hosting.host,
     user:     publishConfig.hosting.username,
-    password: publishConfig.hosting.password
+    password: publishConfig.hosting.password 
   }
 
 
